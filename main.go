36b6b7100b66e8530271feaec03cab0be724b164
pullasuti87 <pullasuti87@gmail.com>
2024-09-16 23:24:32 +0300

    small steps

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
-	"time"
+	// "time"
+	"encoding/hex"
+	"strconv"
 )
 
 /*
-	this is one block
-
+this is one block
 very simplified version
 https://www.investopedia.com/terms/b/block-bitcoin-block.asp
-
 */
 type Block struct {
 	Id        int
@@ -23,4 +23,40 @@ type Block struct {
 
 type Blockchain struct {
 	Chain []*Block
+}
+
+// pointer receiver, reference to block
+func (b *Block) InitHash() {
+	// strconv --> integer to str (integer to ascii)
+	data := strconv.Itoa(b.Id) + strconv.FormatInt(b.Timestamp, 10) + b.PrevHash + b.Data
+	hash := sha256.Sum256([]byte(data))
+	b.Hash = hex.EncodeToString(hash[:])
+}
+
+func FirstBlock() *Block {
+	firstBlock := &Block{
+		Id:        0,
+		Timestamp: 1234567890,
+		PrevHash:  "",
+		Hash:      "",
+		Data:      "",
+	}
+
+	return firstBlock
+}
+
+func CreateBlock() {}
+
+func (bc *Blockchain) AddBlock() {}
+
+func main() {
+	fmt.Printf("test\n")
+
+	//	blockchain := &Blockchain{
+	//		Chain: []*Block{FirstBlock()},
+	//	}
+	//
+	//    blockchain.AddBlock()
+	//    blockchain.AddBlock()
+
 }
